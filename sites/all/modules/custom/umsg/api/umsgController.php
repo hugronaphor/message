0d5dfd947d632daf2fdcebb724c5b44d5da70754
cornel <cornel@c-flex.(none)>
2014-08-03 14:32:09 +0100

    Commit 03.08.v1

--- sites/all/modules/custom/umsg/api/umsgController.php
+++ sites/all/modules/custom/umsg/api/umsgController.php
@@ -22,7 +22,7 @@ class UmsgController { // implements UmsgControllerInterface {
     $this->setUmsgDbActive();
   }
 
-  public function load($scope = 'all', $ids = array(), $account = NULL) {
+  public function load($scope = 'list', $ids = array(), $account = NULL) {
 
     // Get current user by default.
     if ($account === NULL && $this->current_user !== NULL) {
@@ -37,9 +37,12 @@ class UmsgController { // implements UmsgControllerInterface {
     $passed_ids = !is_array($ids) ? $ids : array();
 
     switch ($scope) {
-      case 'all':
-        $query = $this->list_all_threads($account);
-
+      case 'list':
+      case 'list_trash':
+        $query = $this->list_threads($scope, $account);
+        break;
+      case 'list_sent':
+        $query = $this->list_threads($scope, $account);
         break;
 
       default:
@@ -48,27 +51,14 @@ class UmsgController { // implements UmsgControllerInterface {
 
 
     return $query;
-
-    //$query = Database::getConnection('default', 'msgdb'); // dont allow joins
-    $query = db_select('message', 'm');
-    $query->leftJoin('message_index', 'mi', 'mi.mid=m.mid');
-    $query->fields('m');
-    $query->fields('mi');
-    $query->condition('m.author', $account->uid);
-    if (!empty($passed_ids)) {
-      $query->condition('m.mid', $passed_ids, 'IN');
-    }
-    $entities = $query->execute()->fetchAll();
-
-    // To think about structure of hoe the data should be recived.
-    // message can be only one, but inde seems to be only 2.
-    // so: {id, field, [index1, index2]}
-
-
-    return !empty($entities) ? $entities : array();
   }
 
-  private function list_all_threads($account) {
+  private function list_threads($scope, $account) {
+
+    // !!! To figure out for SENT messages.
+    $trash_status = ($scope == 'list_trash') ? 1 : 0;
+    $sent = ($scope == 'list_sent') ? 1 : 0;
+
     $query = db_select('message', 'm')->extend('PagerDefault');
     $query->join('message_index', 'mi', 'mi.mid = m.mid');
 
@@ -76,41 +66,74 @@ class UmsgController { // implements UmsgControllerInterface {
     $count_query = db_select('message', 'm');
     $count_query->addExpression('COUNT(DISTINCT mi.thread_id)', 'count');
     $count_query->join('message_index', 'mi', 'mi.mid = m.mid');
-    $count_query
-      ->condition('mi.recipient', $account->uid)
-      ->condition('mi.deleted', 0);
+    // Sent messages.
+    if ($sent) {
+      $count_query->condition('m.author', $account->uid);
+    }
+    else {
+      $count_query->condition('mi.recipient', $account->uid);
+    }
+    dsm($trash_status, '$trash_status');
+
+    // Trash messages
+    $query->condition('mi.archived', $trash_status);
+    
+    $count_query->condition('mi.deleted', 0);
     $query->setCountQuery($count_query);
-//
-//    dsm($query, 'test');
-//    return;
+
     // Required columns
     $query->addField('mi', 'thread_id');
-    //$query->addExpression('MIN(m.subject)', 'subject');
+    $query->addExpression('SUBSTRING(m.body, 1, 50)', 'subject');
     $query->addExpression('MAX(m.timestamp)', 'last_updated');
     $query->addExpression('SUM(mi.is_new)', 'is_new');
 
     // Load enabled columns
-    $fields = array('participants', 'count', 'thread_started');
+    $fields = array('participants', 'subject');
 
     if (in_array('count', $fields)) {
       // We only want the distinct number of messages in this thread.
-      $query->addExpression('COUNT(distinct mi.mid)', 'count');
+      //$query->addExpression('COUNT(distinct mi.mid)', 'count');
     }
-//    if (in_array('participants', $fields)) {
-//
-//      $query->addExpression("(SELECT GROUP_CONCAT(DISTINCT CONCAT(pmia.type, '_', pmia.recipient))
-//                                     FROM {pm_index} pmia
-//                                     WHERE pmia.type <> 'hidden' AND pmia.thread_id = pmi.thread_id AND pmia.recipient <> :current)", 'participants', array(':current' => $account->uid));
+    if (in_array('participants', $fields)) {
+      // We deal only with one-to-one message (keep this structure for future implementation).
+      $query->addExpression("(SELECT GROUP_CONCAT(DISTINCT CONCAT(mia.recipient, '_', mia.recipient_name))
+                                     FROM {message_index} mia
+                                     WHERE mia.thread_id = mi.thread_id AND mia.recipient <> :current)", 'participants', array(':current' => $account->uid));
+    }
+//!!    if (in_array('thread_started', $fields)) {
+//      $query->addExpression('MIN(m.timestamp)', 'thread_started');
 //    }
-//    if (in_array('thread_started', $fields)) {
-//      $query->addExpression('MIN(pm.timestamp)', 'thread_started');
-//    }
+
+    if ($sent) {
+      $query->condition('m.author', $account->uid);
+    }
+    else {
+      $query->condition('mi.recipient', $account->uid);
+    }
+
+    $query->condition('mi.deleted', 0);
+    $query->condition('mi.archived', $trash_status);
+    $query->groupBy('mi.thread_id');
+    $query->orderBy('last_updated', 'DESC');
+    $query->limit(variable_get('umsg_per_page', 25));
+
+    //dsm($query->execute());
+
+    return $query;
+  }
+
+  public function countUnread($account, $scope) {
+    $trash_status = ($scope == 'trash') ? 1 : 0;
+    
+    $query = db_select('message_index', 'mi');
+    $query->addExpression('COUNT(DISTINCT thread_id)', 'unread_count');
     return $query
-        ->condition('mi.recipient', $account->uid)
-        ->condition('mi.deleted', 0)
-        ->groupBy('mi.thread_id')
-        //->orderByHeader(_privatemsg_list_headers(array_merge(array('subject', 'last_updated'), $fields)))
-        ->limit(variable_get('umsg_per_page', 25));
+                    ->condition('mi.deleted', 0)
+                    ->condition('mi.archived', $trash_status)
+                    ->condition('mi.is_new', 1)
+                    ->condition('mi.recipient', $account->uid)
+                    ->execute()
+                    ->fetchField();
   }
 
   public function save($entity, DatabaseTransaction $transaction = NULL) {
